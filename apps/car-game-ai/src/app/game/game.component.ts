828b3cc2af6c09c2ecc4a129c764b3c99a5d392b
micavanco <micavanco@gmail.com>
2020-03-14 19:28:32 +0100

    Implemented logic to drive car forward, backward and turning to left and right

--- apps/car-game-ai/src/app/game/game.component.ts
+++ apps/car-game-ai/src/app/game/game.component.ts
@@ -10,6 +10,9 @@ export class GameComponent implements AfterViewInit {
 
   @ViewChild("Game") private gameCanvas : ElementRef;
 
+  private context : CanvasRenderingContext2D;
+  private arrowsMap: Object;
+
   @Input() width: number;
   @Input() height: number;
 
@@ -18,9 +21,24 @@ export class GameComponent implements AfterViewInit {
   constructor() {}
 
   ngAfterViewInit(): void {
-    const context = this.gameCanvas.nativeElement.getContext("2d");
-    this.car = new Car(500, 600, 30, 60);
-    this.car.draw(context);
+    this.context = this.gameCanvas.nativeElement.getContext("2d");
+    this.car = new Car(500, 600, 60, 30);
+    this.car.draw(this.context);
+    this.arrowsMap = {};
+    addEventListener('keydown', this.moveCar.bind(this));
+    addEventListener('keyup', this.moveCar.bind(this));
+  }
+
+  moveCar(event) {
+    this.arrowsMap[event.key] = event.type === 'keydown';
+    const carIsTurningTo = this.arrowsMap['ArrowLeft'] ?
+      'ArrowLeft' : this.arrowsMap['ArrowRight'] ? 'ArrowRight' : undefined;
+
+    if(this.arrowsMap['ArrowUp']) {
+      this.car.move(this.context, this.width, this.height, carIsTurningTo, true);
+    } else if(this.arrowsMap['ArrowDown']) {
+      this.car.move(this.context, this.width, this.height, carIsTurningTo, false);
+    }
   }
 
 }
--- apps/car-game-ai/src/app/game/models/car.model.ts
+++ apps/car-game-ai/src/app/game/models/car.model.ts
@@ -3,6 +3,7 @@ export class Car {
 
   private positionX: number;
   private positionY: number;
+  private degrees: number;
   private readonly width: number;
   private readonly height: number;
 
@@ -11,6 +12,7 @@ export class Car {
     this.positionY = positionY;
     this.width = width;
     this.height = height;
+    this.degrees = 90;
   }
 
   draw(context: CanvasRenderingContext2D) {
@@ -23,6 +25,45 @@ export class Car {
 
   set cordinateY(positionY) {
     this.positionY = positionY;
+  }
+
+  move(context: CanvasRenderingContext2D,
+              canvasWidth:number,
+              canvasHeight:number,
+              carIsTurningTo:string,
+              goForward:boolean
+  ) {
+    context.clearRect(0, 0, canvasWidth, canvasHeight);
+    context.save();
+    context.beginPath();
+    this.rotate(context, carIsTurningTo, goForward);
+    context.strokeRect(-this.width/2,-this.height/2, this.width, this.height);
+    context.restore();
+  }
+
+  rotate(context: CanvasRenderingContext2D, carIsTurningTo: string, goForward:boolean) {
+    if(goForward) {
+      if(carIsTurningTo === 'ArrowLeft') {
+        this.degrees -= 1;
+      }
+      if(carIsTurningTo === 'ArrowRight') {
+        this.degrees += 1;
+      }
+      this.positionX-=Math.cos(this.degrees  * Math.PI / 180) * 5;
+      this.positionY-=Math.sin(this.degrees  * Math.PI / 180) * 5;
+    } else {
+      if(carIsTurningTo === 'ArrowLeft') {
+        this.degrees += 1;
+      }
+      if(carIsTurningTo === 'ArrowRight') {
+        this.degrees -= 1;
+      }
+      this.positionX+=Math.cos(this.degrees  * Math.PI / 180) * 5;
+      this.positionY+=Math.sin(this.degrees  * Math.PI / 180) * 5;
+    }
+
+    context.translate( this.positionX + this.width/2, this.positionY + this.height/2 );
+    context.rotate(this.degrees * Math.PI/180)
   }
 
 }
